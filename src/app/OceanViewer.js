b980da854df37ae364cd1d506c6d178fbe30feda
dylan <d.pivo@saeon.nrf.ac.za>
2025-09-26 13:14:01 +0200

    refactor and change favicon

---
+++ src/app/OceanViewer.js
@@ -0,0 +1,180 @@
+'use client'
+
+import React, {useState, useMemo} from 'react';
+import {DeckGL} from '@deck.gl/react';
+import {MVTLayer} from '@deck.gl/geo-layers';
+import {Map} from 'react-map-gl/maplibre';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import {useSearchParams} from 'next/navigation';
+
+import {useDatasetData} from '@/app/DatasetMetadataFetcher';
+import {center, depths, depthMarks, getFillColor, variableColors} from '@/app/MapUtils';
+import DepthSlider from '@/app/DepthSlider';
+import TimeSlider from '@/app/TimeSlider';
+import InfoCard from '@/app/InfoCard';
+import ColourLegend from '@/app/ColourLegend';
+import LayerSelector from '@/app/LayerSelector';
+import {InfoBox} from '@/app/InfoBox';
+import esriMapStyle from '@/app/esri_map_style.json';
+
+export default function OceanViewer() {
+    const searchParams = useSearchParams();
+    const dataset_id = searchParams.get('dataset_id');
+
+    const {data, loading, error, timeMarks, times} = useDatasetData(dataset_id);
+
+    const [viewState, setViewState] = useState({
+        longitude: center[0],
+        latitude: center[1],
+        zoom: 5,
+        pitch: 0,
+        bearing: 0,
+    });
+
+    const initialDepth = 0;
+    const [depth, setDepth] = useState(initialDepth);
+    const [time, setTime] = useState(null);
+    const [selectedLayer, setSelectedLayer] = useState('temperature');
+    const [clickedData, setClickedData] = useState(null);
+    const [showInfoBox, setShowInfoBox] = useState(false);
+    const [selectedFeature, setSelectedFeature] = useState(null);
+    const [isDepthSliderDisabled, setIsDepthSliderDisabled] = useState(false);
+
+    const currentThreshold = useMemo(() => {
+        if (!data) return null;
+        return data.variables.find((v) => v.name === selectedLayer)?.thresholds.find(
+            (t) => t.dependant_value === depth
+        );
+    }, [data, depth, selectedLayer]);
+
+    React.useEffect(() => {
+        if (data && !time) {
+            setTime(data.start_date);
+        }
+    }, [data, time]);
+
+    React.useEffect( () => {
+        if (selectedLayer === 'zeta') {
+            setDepth(0);
+            setIsDepthSliderDisabled(true);
+        } else {
+            setIsDepthSliderDisabled(false);
+        }
+    }, [selectedLayer])
+
+    const martinTileUrl = useMemo(
+        () =>
+            `http://localhost:3001/get_ocean_data_tile/{z}/{x}/{y}?dataset_id=${dataset_id}&date_time=${encodeURIComponent(
+                time
+            )}&depth=${depth}&variable_name=${selectedLayer}`,
+        [depth, time, dataset_id, selectedLayer]
+    );
+
+    const layers = [
+        new MVTLayer({
+            id: 'croco-mvt-layer',
+            data: martinTileUrl,
+            key: martinTileUrl,
+            minZoom: 0,
+            maxZoom: 15,
+            getFillColor: (d) => getFillColor(d, currentThreshold, selectedLayer),
+            getLineColor: (d) => {
+                if (selectedFeature && d.properties.id === selectedFeature.properties.id) {
+                    return [255, 255, 255, 255];
+                }
+                return [0, 0, 0, 0];
+            },
+            getLineWidth: (d) => {
+                if (selectedFeature && d.properties.value === selectedFeature.properties.value) {
+                    return 3;
+                }
+                return 0;
+            },
+            lineWidthUnits: 'pixels',
+            updateTriggers: {
+                getLineColor: [selectedFeature],
+                getLineWidth: [selectedFeature],
+            },
+            autoHighlight: true,
+            pickable: true,
+            onClick: (info) => {
+                if (info.object) {
+                    setClickedData(info);
+                    setShowInfoBox(true);
+                    setSelectedFeature(info.object);
+                } else {
+                    setSelectedFeature(null);
+                }
+            },
+        }),
+    ];
+
+    if (loading || !dataset_id) {
+        return (
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                    backgroundColor: '#1a202c',
+                    color: '#e2e8f0',
+                }}
+            >
+                <Typography variant="body1">Loading data...</Typography>
+            </Box>
+        );
+    }
+
+    if (error) {
+        return (
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                    backgroundColor: '#1a202c',
+                    color: '#f87171',
+                }}
+            >
+                <Typography variant="body1">Error fetching data: {error}</Typography>
+            </Box>
+        );
+    }
+
+    return (
+        <Box sx={{position: 'relative', width: '100vw', height: '100vh'}}>
+            <DeckGL
+                layers={layers}
+                initialViewState={viewState}
+                onViewStateChange={(e) => {
+                    setViewState(e.viewState);
+                }}
+                controller={true}
+            >
+                <Map mapStyle={esriMapStyle}/>
+            </DeckGL>
+            <LayerSelector selectedLayer={selectedLayer} onLayerChange={setSelectedLayer}/>
+            <DepthSlider depth={depth} setDepth={setDepth} depths={depths} depthMarks={depthMarks} disabled={isDepthSliderDisabled}/>
+            <TimeSlider time={time} setTime={setTime} times={times} timeMarks={timeMarks}/>
+            <InfoCard dataset_id={dataset_id} depth={depth} time={time}/>
+            <ColourLegend
+                currentThreshold={currentThreshold}
+                variableColors={variableColors}
+                selectedLayer={selectedLayer}
+            />
+            {showInfoBox && (
+                <InfoBox
+                    data={clickedData}
+                    onClose={() => {
+                        setShowInfoBox(false);
+                        setSelectedFeature(null);
+                    }
+                    }
+                />
+            )}
+        </Box>
+    );
+}
